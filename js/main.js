42bab1a9c0bd1fd68a828e930927bdd4c2261eee
Trasicio Maina <cisiomaina@gmail.com>
2019-01-10 13:23:29 +0300

    Code formating

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@
  * 08-Jan-2019
  * ------------------------------------------------------------------- */
 
-(function($) {
+(function ($) {
 
     "use strict";
-    
+
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
-        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+        scrollDuration: 800, // smoothscroll duration
+        mailChimpURL: 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
 
-    $WIN = $(window);
+        $WIN = $(window);
 
     // Add the User Agent to the <html>
     // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
@@ -21,38 +21,38 @@
     doc.setAttribute('data-useragent', navigator.userAgent);
 
 
-   /* Preloader
-    * -------------------------------------------------- */
-    var clPreloader = function() {
-        
+    /* Preloader
+     * -------------------------------------------------- */
+    var clPreloader = function () {
+
         $("html").addClass('cl-preload');
 
-        $WIN.on('load', function() {
+        $WIN.on('load', function () {
 
             //force page scroll position to top at page refresh
             // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
             // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
+            $("#loader").fadeOut("slow", function () {
                 // will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
-            }); 
-            
+            });
+
             // for hero content animations 
             $("html").removeClass('cl-preload');
             $("html").addClass('cl-loaded');
-        
+
         });
     };
 
 
-   /* Menu on Scrolldown
-    * ------------------------------------------------------ */
-    var clMenuOnScrolldown = function() {
-        
+    /* Menu on Scrolldown
+     * ------------------------------------------------------ */
+    var clMenuOnScrolldown = function () {
+
         var menuTrigger = $('.header-menu-toggle');
 
-        $WIN.on('scroll', function() {
+        $WIN.on('scroll', function () {
 
             if ($WIN.scrollTop() > 150) {
                 menuTrigger.addClass('opaque');
@@ -65,31 +65,31 @@
     };
 
 
-   /* OffCanvas Menu
-    * ------------------------------------------------------ */
-    var clOffCanvas = function() {
+    /* OffCanvas Menu
+     * ------------------------------------------------------ */
+    var clOffCanvas = function () {
 
-        var menuTrigger     = $('.header-menu-toggle'),
-            nav             = $('.header-nav'),
-            closeButton     = nav.find('.header-nav__close'),
-            siteBody        = $('body'),
-            mainContents    = $('section, footer');
+        var menuTrigger = $('.header-menu-toggle'),
+            nav = $('.header-nav'),
+            closeButton = nav.find('.header-nav__close'),
+            siteBody = $('body'),
+            mainContents = $('section, footer');
 
         // open-close menu by clicking on the menu icon
-        menuTrigger.on('click', function(e){
+        menuTrigger.on('click', function (e) {
             e.preventDefault();
             siteBody.toggleClass('menu-is-open');
         });
 
         // close menu by clicking the close button
-        closeButton.on('click', function(e){
+        closeButton.on('click', function (e) {
             e.preventDefault();
             menuTrigger.trigger('click');
         });
 
         // close menu clicking outside the menu itself
-        siteBody.on('click', function(e){
-            if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span') ) {
+        siteBody.on('click', function (e) {
+            if (!$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span')) {
                 siteBody.removeClass('menu-is-open');
             }
         });
@@ -97,31 +97,31 @@
     };
 
 
-   /* photoswipe
-    * ----------------------------------------------------- */
-    var clPhotoswipe = function() {
+    /* photoswipe
+     * ----------------------------------------------------- */
+    var clPhotoswipe = function () {
         var items = [],
             $pswp = $('.pswp')[0],
             $folioItems = $('.item-folio');
 
         // get items
-        $folioItems.each( function(i) {
+        $folioItems.each(function (i) {
 
             var $folio = $(this),
-                $thumbLink =  $folio.find('.thumb-link'),
+                $thumbLink = $folio.find('.thumb-link'),
                 $title = $folio.find('.item-folio__title'),
                 $caption = $folio.find('.item-folio__caption'),
                 $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
                 $captionText = $.trim($caption.html()),
                 $href = $thumbLink.attr('href'),
                 $size = $thumbLink.data('size').split('x'),
-                $width  = $size[0],
+                $width = $size[0],
                 $height = $size[1];
-        
+
             var item = {
-                src  : $href,
-                w    : $width,
-                h    : $height
+                src: $href,
+                w: $width,
+                h: $height
             }
 
             if ($caption.length > 0) {
@@ -132,9 +132,9 @@
         });
 
         // bind click event
-        $folioItems.each(function(i) {
+        $folioItems.each(function (i) {
 
-            $(this).on('click', function(e) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 var options = {
                     index: i,
@@ -150,16 +150,16 @@
     };
 
 
-   /* Stat Counter
-    * ------------------------------------------------------ */
-    var clStatCount = function() {
-        
+    /* Stat Counter
+     * ------------------------------------------------------ */
+    var clStatCount = function () {
+
         var statSection = $(".s-stats"),
             stats = $(".stats__count");
 
         statSection.waypoint({
 
-            handler: function(direction) {
+            handler: function (direction) {
 
                 if (direction === "down") {
 
@@ -175,7 +175,7 @@
                         });
                     });
 
-                } 
+                }
 
                 // trigger once only
                 this.destroy();
@@ -188,10 +188,10 @@
     };
 
 
-   /* Masonry
-    * ---------------------------------------------------- */ 
+    /* Masonry
+     * ---------------------------------------------------- */
     var clMasonryFolio = function () {
-        
+
         var containerBricks = $('.masonry');
 
         containerBricks.imagesLoaded(function () {
@@ -202,7 +202,7 @@
         });
 
         // layout Masonry after each image loads
-        containerBricks.imagesLoaded().progress( function() {
+        containerBricks.imagesLoaded().progress(function () {
             containerBricks.masonry('layout');
         });
     };
@@ -210,8 +210,8 @@
 
     /* slick slider
      * ------------------------------------------------------ */
-    var clSlickSlider = function() {
-        
+    var clSlickSlider = function () {
+
         $('.testimonials__slider').slick({
             arrows: false,
             dots: true,
@@ -233,16 +233,16 @@
     };
 
 
-   /* Smooth Scrolling
-    * ------------------------------------------------------ */
-    var clSmoothScroll = function() {
-        
+    /* Smooth Scrolling
+     * ------------------------------------------------------ */
+    var clSmoothScroll = function () {
+
         $('.smoothscroll').on('click', function (e) {
             var target = this.hash,
-            $target    = $(target);
-            
-                e.preventDefault();
-                e.stopPropagation();
+                $target = $(target);
+
+            e.preventDefault();
+            e.stopPropagation();
 
             $('html, body').stop().animate({
                 'scrollTop': $target.offset().top
@@ -260,29 +260,29 @@
     };
 
 
-   /* Placeholder Plugin Settings
-    * ------------------------------------------------------ */
-    var clPlaceholder = function() {
-        $('input, textarea, select').placeholder();  
+    /* Placeholder Plugin Settings
+     * ------------------------------------------------------ */
+    var clPlaceholder = function () {
+        $('input, textarea, select').placeholder();
     };
 
 
-   /* Alert Boxes
-    * ------------------------------------------------------ */
-    var clAlertBoxes = function() {
+    /* Alert Boxes
+     * ------------------------------------------------------ */
+    var clAlertBoxes = function () {
 
-        $('.alert-box').on('click', '.alert-box__close', function() {
+        $('.alert-box').on('click', '.alert-box__close', function () {
             $(this).parent().fadeOut(500);
-        }); 
+        });
 
     };
 
 
-   /* Animate On Scroll
-    * ------------------------------------------------------ */
-    var clAOS = function() {
-        
-        AOS.init( {
+    /* Animate On Scroll
+     * ------------------------------------------------------ */
+    var clAOS = function () {
+
+        AOS.init({
             offset: 200,
             duration: 600,
             easing: 'ease-in-sine',
@@ -294,10 +294,10 @@
     };
 
 
-   /* AjaxChimp
-    * ------------------------------------------------------ */
-    var clAjaxChimp = function() {
-        
+    /* AjaxChimp
+     * ------------------------------------------------------ */
+    var clAjaxChimp = function () {
+
         $('#mc-form').ajaxChimp({
             language: 'es',
             url: cfg.mailChimpURL
@@ -322,23 +322,23 @@
             3: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
             4: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
             5: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.'
-        } 
+        }
 
     };
 
 
-   /* Back to Top
-    * ------------------------------------------------------ */
-    var clBackToTop = function() {
-        
-        var pxShow  = 500,         // height on which the button will show
-        fadeInTime  = 400,         // how slow/fast you want the button to show
-        fadeOutTime = 400,         // how slow/fast you want the button to hide
-        scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
-        goTopButton = $(".cl-go-top")
-        
+    /* Back to Top
+     * ------------------------------------------------------ */
+    var clBackToTop = function () {
+
+        var pxShow = 500,         // height on which the button will show
+            fadeInTime = 400,         // how slow/fast you want the button to show
+            fadeOutTime = 400,         // how slow/fast you want the button to hide
+            scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+            goTopButton = $(".cl-go-top")
+
         // Show or hide the sticky footer button
-        $(window).on('scroll', function() {
+        $(window).on('scroll', function () {
             if ($(window).scrollTop() >= pxShow) {
                 goTopButton.fadeIn(fadeInTime);
             } else {
@@ -348,10 +348,10 @@
     };
 
 
-   /* Initialize
-    * ------------------------------------------------------ */
+    /* Initialize
+     * ------------------------------------------------------ */
     (function clInit() {
-        
+
         clPreloader();
         clMenuOnScrolldown();
         clOffCanvas();
@@ -367,7 +367,7 @@
         clBackToTop();
 
     })();
-        
+
 })(jQuery);
 
 // End
--- js/map.js
+++ js/map.js
@@ -6,97 +6,97 @@
 
 var map = null, infobox, dataLayer;
 
-  function GetMap() 
-  {
-      map = new Microsoft.Maps.Map(document.getElementById("myMap"),
-         {
+function GetMap() {
+    map = new Microsoft.Maps.Map(document.getElementById("myMap"),
+        {
             credentials: "Ap9gcOqVjTE1TIN5S3RUdmmjNK1aSWdkVHEMAQAVvFAMBMwNlLhzGI6XuxtVDEYU",
-            center: new Microsoft.Maps.Location(40, -1)});
-     dataLayer = new Microsoft.Maps.EntityCollection();
-     map.entities.push(dataLayer);
-     map.setView({ zoom: 5 });
-   var infoboxLayer = new Microsoft.Maps.EntityCollection();
-     map.entities.push(infoboxLayer);
-     infobox = new Microsoft.Maps.Infobox();
-     infoboxLayer.push(infobox);
-     AddData();
-  }
+            center: new Microsoft.Maps.Location(40, -1)
+        });
+    dataLayer = new Microsoft.Maps.EntityCollection();
+    map.entities.push(dataLayer);
+    map.setView({ zoom: 5 });
+    var infoboxLayer = new Microsoft.Maps.EntityCollection();
+    map.entities.push(infoboxLayer);
+    infobox = new Microsoft.Maps.Infobox();
+    infoboxLayer.push(infobox);
+    AddData();
+}
 
 function AddData() {
-//Below is the data arranged as (lat , long) and description values (where they've been given).
+    //Below is the data arranged as (lat , long) and description values (where they've been given).
     var pin1 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.78334, 39.670486));
     pin1.Title = "Kizingo nursery";
     pin1.Description = "Pin 1 description";
     Microsoft.Maps.Events.addHandler(pin1, 'click', displayInfobox);
     dataLayer.push(pin1);
 
-    var pin2 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-1.49397130662931, 39.6826171875, ));
+    var pin2 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-1.49397130662931, 39.6826171875));
     pin2.Title = "Kamatish's School";
     pin2.Description = "Pin 2 description";
     Microsoft.Maps.Events.addHandler(pin2, 'click', displayInfobox);
     dataLayer.push(pin2);
 
-var pin3 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(6.31529853833003, 33.22265625));
+    var pin3 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(6.31529853833003, 33.22265625));
     pin3.Title = "Test after commenting";
     pin3.Description = "Pin 3 description";
     Microsoft.Maps.Events.addHandler(pin3, 'click', displayInfobox);
     dataLayer.push(pin3);
 
-var pin4 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(7.044651, 38.487855));
+    var pin4 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(7.044651, 38.487855));
     pin4.Title = "ict";
     pin4.Description = "Pin 4 description";
     Microsoft.Maps.Events.addHandler(pin4, 'click', displayInfobox);
     dataLayer.push(pin4);
 
-var pin5 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(3.16245553023785, 39.90234375));
+    var pin5 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(3.16245553023785, 39.90234375));
     pin5.Title = "Test Add Data";
     pin5.Description = "Pin 5 description";
     Microsoft.Maps.Events.addHandler(pin5, 'click', displayInfobox);
     dataLayer.push(pin5);
 
-var pin6 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(6.66460756217257, 12.83203125));
+    var pin6 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(6.66460756217257, 12.83203125));
     pin6.Title = "Mercy Image test";
     pin6.Description = "Pin 6 description";
     Microsoft.Maps.Events.addHandler(pin6, 'click', displayInfobox);
     dataLayer.push(pin6);
 
-var pin7 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-0.703107352436478, 34.98046875));
+    var pin7 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-0.703107352436478, 34.98046875));
     pin7.Title = "Kizingo nursery";
     pin7.Description = "Pin 7 description";
     Microsoft.Maps.Events.addHandler(pin7, 'click', displayInfobox);
     dataLayer.push(pin7);
 
-var pin8 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.485907, 39.88699));
+    var pin8 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.485907, 39.88699));
     pin8.Title = "Kizingo nursery";
     pin8.Description = "Pin 8 description";
     Microsoft.Maps.Events.addHandler(pin8, 'click', displayInfobox);
     dataLayer.push(pin8);
 
-var pin9 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.785518, 39.682212));
+    var pin9 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.785518, 39.682212));
     pin9.Title = "Kizingo nursery";
     pin9.Description = "Pin 9 description";
     Microsoft.Maps.Events.addHandler(pin9, 'click', displayInfobox);
     dataLayer.push(pin9);
 
-var pin10 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.479406, 39.744492));
+    var pin10 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.479406, 39.744492));
     pin10.Title = "Kizingo nursery";
     pin10.Description = "Pin 10 description";
     Microsoft.Maps.Events.addHandler(pin10, 'click', displayInfobox);
     dataLayer.push(pin10);
 
-var pin11 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.438276, 39.766075));
+    var pin11 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.438276, 39.766075));
     pin11.Title = "Kizingo nursery";
     pin11.Description = "Pin 11 description";
     Microsoft.Maps.Events.addHandler(pin11, 'click', displayInfobox);
     dataLayer.push(pin11);
 
-var pin12 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.464137, 39.694638));
+    var pin12 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.464137, 39.694638));
     pin12.Title = "Kizingo nursery";
     pin12.Description = "Pin 12 description";
     Microsoft.Maps.Events.addHandler(pin12, 'click', displayInfobox);
     dataLayer.push(pin12);
 
-var pin13 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.475631, 39.92941));
+    var pin13 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.475631, 39.92941));
     pin13.Title = "Kizingo nursery";
     pin13.Description = "Pin 13 description";
     Microsoft.Maps.Events.addHandler(pin13, 'click', displayInfobox);
@@ -156,7 +156,7 @@ var pin13 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-3.475631, 39
 function displayInfobox(e) {
     if (e.targetType == 'pushpin') {
         infobox.setLocation(e.target.getLocation());
-                 
+
         infobox.setOptions({
             visible: true,
             title: e.target.Title,
